f11f22e055e42a45061291509faf1185fc268f0d
SinaDavid <s.david@vu.nl>
2024-09-16 13:01:11 +0200

    Update reader.py
    adjusted delimiter choice

--- reader.py
+++ reader.py
@@ -7,10 +7,10 @@ Created on Wed Jul  3 17:22:24 2024
 import csv
 import numpy as np
 
-def read_headers(filename):
+def read_headers(filename, delimiter=','):
     with open(filename, 'r') as f:
-        headers = f.readline().strip().split(',')
+        headers = f.readline().strip().split(delimiter)
     return headers
 
-def read_data(filename):
-    return np.loadtxt(filename, delimiter=',', skiprows=1)
+def read_data(filename, delimiter=','):
+    return np.loadtxt(filename, delimiter=delimiter, skiprows=1)
